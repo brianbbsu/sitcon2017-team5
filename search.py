fb95dee06810a798e4359509f3343fcf60dbd8fe
briansu <brianbb.su@gmail.com>
2017-08-16 23:14:33 +0800

    fixed gui.py

--- search.py
+++ search.py
@@ -1,9 +1,9 @@
 import requests
 import json
-from pprint import pprint
 import urllib
 from bot import read
 from geopy.distance import vincenty
+from pprint import pprint
 
 base="https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=***REMOVED***&rankby=distance&opennow=1"
 
@@ -24,13 +24,15 @@ def get_search(lat,lng,tp):
 			tmp["long"]=dt["geometry"]["location"]["lng"]
 			tmp["name"]=dt["name"]
 			tmp["id"]=dt["place_id"]
-			tmp["dis"]=vincenty((lat,lng),(tmp["lat"],tmp["long"])).meters
+			tmp["dis"]=int(vincenty((lat,lng),(tmp["lat"],tmp["long"])).meters)
 			rt.append(tmp)
 		pprint(rt)
-
+	return rt
+'''
 while 1:
 	data=read()
 	if data==None or data["type"]!="location":
 		continue
 	get_search(data["lat"],data["long"],"restaurant")
 	break
+'''
