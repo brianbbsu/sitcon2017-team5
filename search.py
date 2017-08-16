245509879245ed91b47693ad958f8d8a6c0781da
briansu <brianbb.su@gmail.com>
2017-08-16 20:13:26 +0800

    Add search.py for searching nearby

---
+++ search.py
@@ -0,0 +1,20 @@
+import requests
+import json
+from pprint import pprint
+import urllib
+from bot import read
+
+base="https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=***REMOVED***&rankby=distance"
+
+def get_search(lat,lng,tp):
+	place="&location="+str(lat)+","+str(lng)+"&type="+tp
+	raw=requests.get(base+place)
+	js=json.loads(raw.text)
+	pprint(js)
+
+while 1:
+	data=read()
+	if data==None or data["type"]!="location":
+		continue
+	get_search(data["lat"],data["long"],"restaurant")
+	break
