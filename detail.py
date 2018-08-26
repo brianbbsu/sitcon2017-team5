33f1669d1263a1f896de6a705763cbbac1746ce4
briansu <brianbb.su@gmail.com>
2018-08-26 12:08:45 +0800

    Showing opening time

--- detail.py
+++ detail.py
@@ -1,11 +1,16 @@
 import requests
 import json
 import urllib
+import datetime
+from tzwhere import tzwhere
+import pytz
 from bot import read
 from pprint import pprint
 import conf
 
 base="https://maps.googleapis.com/maps/api/place/details/json?language=zh-TW&key="+conf.googlemap_apikey+"&placeid="
+
+tz = tzwhere.tzwhere()
 
 def get_detail(pid):
 	raw=requests.get(base+str(pid))
@@ -14,6 +19,9 @@ def get_detail(pid):
 	print(base+str(pid))
 	
 	rt={}
+
+	rt["lat"]=js["result"]["geometry"]["location"]["lat"]
+	rt["long"]=js["result"]["geometry"]["location"]["lng"]
 
 	if "formatted_address" in js["result"]:
 		rt["add"]=js["result"]["formatted_address"]
@@ -42,6 +50,14 @@ def get_detail(pid):
 			rt["open"] = "營業中"
 		else:
 			rt["open"] = "休息中"
+		
+		if "weekday_text" in js["result"]["opening_hours"]:
+			txt = ["日","一","二","三","四","五","六"]
+			tz_str = tz.tzNameAt(float(rt['lat']),float(rt['long']))
+			weekday = int(datetime.datetime.now(pytz.timezone(tz_str)).strftime('%w'))
+			for s in js["result"]["opening_hours"]["weekday_text"]:
+				if s[2] == txt[weekday]:
+					rt["open"] += " (" + s + ")"
 	else:
 		rt["open"] = "未知"
 	
